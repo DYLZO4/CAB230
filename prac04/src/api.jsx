5999822664a70f1b2e0ddf6365257f35c901124e
Dylan Hessing <dylanhessing@hotmail.com>
2025-03-19 14:16:01 +1000

    hidden api key

---
+++ prac04/src/api.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+
+const API_KEY = import.meta.env.VITE_APIKEY;
+
+const QUERY = "Brisbane";
+
+export function useWeather() {
+    const [loading, setLoading] = useState(true);
+    const [headlines, setHeadlines] = useState([]);
+    const [error, setError] = useState(null);
+  
+    useEffect(() => {
+      getForecastByQuery(QUERY)
+        .then((headlines) => {
+          setHeadlines(headlines);
+        })
+        .catch((e) => {
+          setError(e);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    }, []);
+  
+    return {
+      loading,
+      headlines,
+      error
+    };
+  }
+
+    function getForecastByQuery(q) {
+        const url = `https://api.weatherapi.com/v1/forecast.json?q=${q}&key=${API_KEY}`;
+        return fetch(url)
+          .then((res) => res.json())
+          .then((res) => res.forecast.forecastday[0].hour)
+          .then((forecasts) =>
+            forecasts.map((forecast) => ({
+              time: forecast.time,
+              text: forecast.condition.text,
+              temp: forecast.temp_c,
+              wind: forecast.wind_kph
+            }))
+          );
+      }
